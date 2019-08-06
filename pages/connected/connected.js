56f964c8af5e21390461a84ea51a2e3a1aff7469
arfrendan <sjx87938@gmail.com>
2019-08-06 18:16:02 +0800

    0608

--- pages/connected/connected.js
+++ pages/connected/connected.js
@@ -1,4 +1,17 @@
 // pages/connected/connected.js
+var base64arraybuffer = require('../../utils/base64-arraybuffer')
+var gb2312ToBase64 = require('../../utils/gb2312ToBase64')
+var deviceID =''
+function stringToBuffer(string){
+  let data_base64 = gb2312ToBase64.encode64(string);
+  let buffer = base64arraybuffer.decode(data_base64);
+  return buffer;
+}
+function bufferToString(buffer){
+  let data_base64 = base64arraybuffer.encode(buffer);
+  let string = gb2312ToBase64.decode64(data_base64);
+  return string;
+}
 Page({
 
   /**
@@ -6,7 +19,9 @@ Page({
    */
   data: {
     chosenName: "now device is",
-    chosenID: ""
+    chosenID: "",
+    serviceUUID: "",
+    characteristicsUUID:""
   },
 
 
@@ -15,21 +30,66 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    var that = this;
     this.setData({ chosenID: options.deviceID})
+    
+    wx.getBLEDeviceServices({
+      deviceId: options.deviceID,
+      success(res) {
+        console.log('device services:', res.services)
+        that.setData({serviceUUID: res.services[0].uuid})
+        wx.getBLEDeviceCharacteristics({
+          deviceId : options.deviceID,
+          serviceId: res.services[0].uuid,
+          success(res) {
+            console.log('device getBLEDeviceCharacteristics:', res.characteristics)
+            that.setData({ characteristicsUUID: res.characteristics[0].uuid })
+            wx.notifyBLECharacteristicValueChange({
+              state: true,
+              deviceId: that.data.chosenID,
+              serviceId: that.data.serviceUUID,
+              characteristicId: that.data.characteristicsUUID,
+              success(res) {
+                console.log('notifyBLECharacteristicValueChange success', res.errMsg)
+              }
+            })
+          }
+        })
+      }
+    })
   },
 
+  changeState: function(){
+    var that = this;
+    let data = 'change';
+    buffer = stringToBuffer(data)
+    
+    wx.writeBLECharacteristicValue({
+      deviceId : that.data.chosenID,
+      serviceId : that.data.serviceUUID,
+      characteristicId : that.data.characteristicsUUID,
+      value: buffer,
+      success(res) {
+        console.log('writeBLECharacteristicValue success', res.errMsg)
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+    
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+    wx.onBLECharacteristicValueChange(function (res) {
+      console.log(`characteristic ${res.characteristicId} has changed, now is ${res.value}`)
+      let recData = bufferToString(res.value)
+      
+    })
   },
 
   /**
